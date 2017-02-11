ad32311b6dc7aad0e273532de69a6be913cec85c
redbattousai <kchangfatt@gmail.com>
2017-02-11 06:37:07 +0000

    Added search feature.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { RouterModule }   from '@angular/router';
-import { HttpModule }    from '@angular/http';
+import { HttpModule }     from '@angular/http';
 
 import { AppRoutingModule }     from './app-routing.module';
 
@@ -15,6 +15,7 @@ import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { DashboardComponent }  from './dashboard.component';
 import { HeroService }         from './hero.service';
+import { HeroSearchComponent } from './hero-search.component';
 
 
 @NgModule({
@@ -27,9 +28,10 @@ import { HeroService }         from './hero.service';
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    HeroSearchComponent
   ],
   providers: [ HeroService ],
   bootstrap: [ AppComponent ]
